60ee70d6801b7ba05429f017e378a71c7ea15d38
Sithu Aung <sithu_aung@intuit.com>
2018-03-08 14:09:34 -0800

    add more voter fields

--- src/components/voter-reg-form.js
+++ src/components/voter-reg-form.js
@@ -25,10 +25,6 @@ export class VoterRegForm extends React.Component {
         this.setState({modalIsOpen: true});
     }
 
-    afterOpenModal = () => {
-        this.subtitle.style.color = '#f00';
-    }
-
     closeModal = () => {
         this.setState({modalIsOpen: false});
     }
@@ -36,8 +32,12 @@ export class VoterRegForm extends React.Component {
     save = () => {
         console.log('saving' + this.name.value);
         const voter = {
-            first_name: this.name.value,
-        }
+            first_name: this.fname.value,
+            last_name: this.lname.value,
+            address: this.address.value,
+            city: this.city.value,
+            birthdate: this.birthdate.value
+        };
         this.props.onSave(voter);
         this.closeModal();
     }
@@ -47,20 +47,33 @@ export class VoterRegForm extends React.Component {
             <div>
                 <button onClick={this.openModel}>Register</button>
                 <Modal
-                isOpen={this.state.modalIsOpen}
-                onAfterOpen={this.afterOpenModal}
-                onRequestClose={this.closeModal}
-                style={customStyles}
-                contentLabel="Voter Registration Form"
-                >
-        
+                    isOpen={this.state.modalIsOpen}
+                    onRequestClose={this.closeModal}
+                    style={customStyles}
+                    contentLabel="Voter Registration Form"
+                    >
                 <h2 ref={subtitle => this.subtitle = subtitle}>Voter Registration Form</h2>
                 <button onClick={this.closeModal}>Close</button>
                 <form>
                     <div>
-                    <label htmlFor="name-input">Name:</label>
-                    <input id="name-input" type="text" name="name" 
-                        ref={n => this.name = n} />
+                        <label htmlFor="fname-input">First Name:</label>
+                        <input id="fname-input" type="text" name="fname" ref={n => this.fname = n} />
+                    </div>
+                    <div>
+                        <label htmlFor="lname-input">Last Name:</label>
+                        <input id="lname-input" type="text" name="lname" ref={n => this.lname = n} />
+                    </div>
+                    <div>
+                        <label htmlFor="address-input">Address:</label>
+                        <input id="address-input" type="text" name="address" ref={n => this.address = n} />
+                    </div>
+                    <div>
+                        <label htmlFor="city-input">City:</label>
+                        <input id="city-input" type="text" name="city" ref={n => this.city = n} />
+                    </div>
+                    <div>
+                        <label htmlFor="bd-input">Birthdate:</label>
+                        <input id="db-input" type="text" name="birthdate" ref={n => this.birthdate = n} />
                     </div>
                     <button type="button" onClick={this.save}>Save</button>
                 </form>

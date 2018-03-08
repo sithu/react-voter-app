1e917b04f440fa9081c79b3399f2c15baddcb405
Sithu Aung <sithu_aung@intuit.com>
2018-03-08 10:00:56 -0800

    add voter component to voter-main

--- src/components/voter-main.js
+++ src/components/voter-main.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as PropTypes from 'prop-types';
 
 import { Header } from './header';
+import { Voter } from './voter';
 
 export class VoterMain extends React.Component {
     static defaultProps = {
@@ -11,14 +12,13 @@ export class VoterMain extends React.Component {
 
     constructor(props) {
         super(props);
-        console.log('--- constructor ---');
     }
 
     render() {
         return (
             <div>
                 <Header headerText="Voter Menu" />
-                <h3>Placeholder for voter registration Component</h3>
+                <Voter voters={this.props.voters} />
                 <h3>Placeholder for Select Election and Voting Components</h3>
                 <h3>Placeholder for New Election Component</h3>
                 <h3>Placeholder for Election Result Component</h3>
---
+++ src/components/voter.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+export class Voter extends React.Component {
+    render() {
+        return (
+            <button type="button">Register</button>
+        );
+    }
+}

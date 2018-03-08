b7cd36968287aa3207cf08a6e54f3ffb74306888
Pignetti <mpignetti@MTVL16092be9c.local>
2018-03-08 14:05:55 -0800

    adding election actions and importing them to index

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import { VoterMain } from './components/voter-main';
  * Custom Actions imports
  */
 import { refreshVoter, insertVoter, updateVoter, deleteVoter } from './actions/voter-reg-actions';
+import { insertElection } from './actions/election-actions';
 
 
 const MainContainer = connect(
